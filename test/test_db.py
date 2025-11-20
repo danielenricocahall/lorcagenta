7366547e3f72ff782c321b4f8c94313f67ccce37
daniel.cahall <danielenricocahall@gmail.com>
2025-11-19 21:03:10 -0500

    fix test name

--- test/test_db.py
+++ test/test_db.py
@@ -3,6 +3,6 @@ import pytest
 
 
 @pytest.mark.parametrize("store_type", ["sqlite", "postgres"])
-def test_sqlite_card_store(store_type: str):
+def test_card_store_rdbms(store_type: str):
     card_store = CardStore.get_card_store(store_type)
     assert "lorcana_cards" in card_store.tables
