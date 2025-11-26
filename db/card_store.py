683f2f77e813f8d9c4e4eca94b189f65d863f1d7
daniel.cahall <danielenricocahall@gmail.com>
2025-11-25 21:50:04 -0500

    feature: revert queries, abstract

--- db/card_store.py
+++ db/card_store.py
@@ -1,9 +1,11 @@
 import os
+import sqlite3
 from abc import ABC, abstractmethod
 from itertools import chain
 
 from pysqlscribe.table import Table
 from pysqlscribe.utils.ddl_loader import load_tables_from_ddls
+from pysqlscribe.scalar_functions import lower
 
 file_path = os.path.dirname(__file__)
 project_root = os.path.dirname(file_path)
@@ -27,6 +29,26 @@ class CardStore(ABC):
         except StopIteration:
             raise ValueError(f"Unsupported card store type: {type}")
 
+    @abstractmethod
+    def get_cards(
+        self, color: str | None = None, name: str | None = None, cost: int | None = None
+    ) -> list[dict]: ...
+
+    @property
+    def color_to_id_mapping(self):
+        return {
+            "ruby": 1,
+            "sapphire": 2,
+            "emerald": 3,
+            "amber": 4,
+            "amethyst": 5,
+            "steel": 6,
+        }
+
+    @property
+    def id_to_color_mapping(self):
+        return {v: k for k, v in self.card_number_to_color_mapping.items()}
+
 
 class CardStoreRDBMS(CardStore):
     _type: str = "rdbms"
@@ -36,11 +58,30 @@ class CardStoreRDBMS(CardStore):
 
     @property
     def card_table(self) -> Table:
-        return self.tables["cards"]
+        return self.tables["lorcana_cards"]
 
     @property
     @abstractmethod
     def connection_string(self) -> str: ...
+
+    def get_cards(
+        self, color: str | None = None, name: str | None = None, cost: int | None = None
+    ) -> list[dict]:
+        query = self.card_table.select("*")
+        clauses = []
+        if color:
+            clauses.append(self.card_table.color == self.color_to_id_mapping[color])
+        if cost is not None:
+            clauses.append(self.card_table.cost == cost)
+        if name:
+            clauses.append(lower(self.card_table.name) == name.lower())
+        if clauses:
+            query = query.where(*clauses)
+        return self._get_cards_from_db(str(query))
+
+    @abstractmethod
+    def _get_cards_from_db(self, query: str) -> list[dict]:
+        raise NotImplementedError("Subclasses must implement this method")
 
 
 class CardStoreInMemory(CardStore):
@@ -52,7 +93,15 @@ class CardStoreSQLite(CardStoreRDBMS):
 
     @property
     def connection_string(self) -> str:
-        return "sqlite:///:memory:"
+        return os.path.join(project_root, "cards.db")
+
+    def _get_cards_from_db(self, query: str) -> list[dict]:
+        with sqlite3.connect(self.connection_string) as conn:
+            cursor = conn.cursor()
+            cursor.execute(str(query))
+            rows = cursor.fetchall()
+            columns = [desc[0] for desc in cursor.description]
+            return [dict(zip(columns, row)) for row in rows]
 
 
 class CardStorePostgres(CardStoreRDBMS):
@@ -70,3 +119,9 @@ class CardStorePostgres(CardStoreRDBMS):
             getenv("POSTGRES_DB"),
         )
         return f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}"
+
+
+if __name__ == "__main__":
+    card_store = CardStore.get_card_store("sqlite")
+    cards = card_store.get_cards(color="sapphire", name="nick wilde")
+    print(f"Retrieved {len(cards)} cards")
